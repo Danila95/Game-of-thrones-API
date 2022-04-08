a7c7ffa7e3e251fcff303d49a3549f025cc47c31
ddb4 <ddb4@tpu.ru>
2022-04-08 16:25:51 +0700

    add content

--- src/components/common.components/app/App.jsx
+++ src/components/common.components/app/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
 
 	return (
 		<div className="wrapper">
-			<div className="app">
+			{/* <div className="app">
 				<Header/>
 				<main>
 					<RandomChar/>
@@ -19,7 +19,23 @@ const App = () => {
 					</div>
 					<img className="bg-decoration" src={decoration} alt="vision"/>
 				</main>
+			</div> */}
+			{/* background-video  */}
+			<div class="full-screen">
+				<div class="full-screen__body">
+					<div class="full-screen__title">TEST</div>
+						{/* Use a button to pause/play the video with JavaScript  */}
+						<button id="myBtn" onClick="myFunction()">Pause</button>
+						<button id="myBtn2" onClick="myAudio()">Play</button>
+				</div>
 			</div>
+			<video autoplay muted loop preload="auto" class="background-video" id="myVideo">
+				<source src="videos/background-video.webm" type="video/webm"></source>
+				<source src="videos/background-video.mp4" type="video/mp4"></source>
+			</video>
+			<audio autoplay loop id="myAudio">
+				<source src="audio/main-theme-got.mp3" type="audio/mp3"></source>
+			</audio>
 		</div>
 	)
 };
